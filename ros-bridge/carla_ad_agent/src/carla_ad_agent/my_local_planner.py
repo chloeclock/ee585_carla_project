8eea2ec9813464c585f05a429baa3799f9327da0
Mengyi <78004590+chloeclock@users.noreply.github.com>
2021-12-19 17:35:01 +0900

    Update my_local_planner.py

--- ros-bridge/carla_ad_agent/src/carla_ad_agent/my_local_planner.py
+++ ros-bridge/carla_ad_agent/src/carla_ad_agent/my_local_planner.py
@@ -11,6 +11,7 @@ low-level waypoint following based on PID controllers.
 """
 
 from collections import deque
+import copy
 import rospy
 import math
 import numpy as np
@@ -24,6 +25,262 @@ from vehicle_pid_controller import VehiclePIDController  # pylint: disable=relat
 from misc import distance_vehicle  # pylint: disable=relative-import
 import carla
 import carla_ros_bridge.transforms as trans
+
+# parameters
+SIM_LOOP = 100
+
+# Parameter
+MAX_SPEED = 50.0 / 3.6  # maximum speed [m/s]
+MAX_ACCEL = 2.0  # maximum acceleration [m/ss]
+MAX_CURVATURE = 1.0  # maximum curvature [1/m]
+MAX_ROAD_WIDTH = 7.0  # maximum road width [m]
+D_ROAD_W = 1.0  # road width sampling length [m]
+DT = 0.2  # time tick [s]
+MAX_T = 5.0  # max prediction time [m]
+MIN_T = 4.0  # min prediction time [m]
+TARGET_SPEED = 30.0 / 3.6  # target speed [m/s]
+D_T_S = 5.0 / 3.6  # target speed sampling length [m/s]
+N_S_SAMPLE = 1  # sampling number of target speed
+ROBOT_RADIUS = 2.0  # robot radius [m]
+
+# cost weights
+K_J = 0.1
+K_T = 0.1
+K_D = 1.0
+K_LAT = 1.0
+K_LON = 1.0
+
+class QuinticPolynomial:
+
+    def __init__(self, xs, vxs, axs, xe, vxe, axe, time):
+        # calc coefficient of quintic polynomial
+        # See jupyter notebook document for derivation of this equation.
+        self.a0 = xs
+        self.a1 = vxs
+        self.a2 = axs / 2.0
+
+        A = np.array([[time ** 3, time ** 4, time ** 5],
+                      [3 * time ** 2, 4 * time ** 3, 5 * time ** 4],
+                      [6 * time, 12 * time ** 2, 20 * time ** 3]])
+        b = np.array([xe - self.a0 - self.a1 * time - self.a2 * time ** 2,
+                      vxe - self.a1 - 2 * self.a2 * time,
+                      axe - 2 * self.a2])
+        x = np.linalg.solve(A, b)
+
+        self.a3 = x[0]
+        self.a4 = x[1]
+        self.a5 = x[2]
+
+    def calc_point(self, t):
+        xt = self.a0 + self.a1 * t + self.a2 * t ** 2 + \
+             self.a3 * t ** 3 + self.a4 * t ** 4 + self.a5 * t ** 5
+
+        return xt
+
+    def calc_first_derivative(self, t):
+        xt = self.a1 + 2 * self.a2 * t + \
+             3 * self.a3 * t ** 2 + 4 * self.a4 * t ** 3 + 5 * self.a5 * t ** 4
+
+        return xt
+
+    def calc_second_derivative(self, t):
+        xt = 2 * self.a2 + 6 * self.a3 * t + 12 * self.a4 * t ** 2 + 20 * self.a5 * t ** 3
+
+        return xt
+
+    def calc_third_derivative(self, t):
+        xt = 6 * self.a3 + 24 * self.a4 * t + 60 * self.a5 * t ** 2
+
+        return xt
+
+# (self, xs, vxs, axs, vxe, axe, time) polynomial approximation 
+class QuarticPolynomial:
+
+    def __init__(self, xs, vxs, axs, vxe, axe, time):
+        # calc coefficient of quartic polynomial
+
+        self.a0 = xs
+        self.a1 = vxs
+        self.a2 = axs / 2.0
+
+        A = np.array([[3 * time ** 2, 4 * time ** 3],
+                      [6 * time, 12 * time ** 2]])
+        b = np.array([vxe - self.a1 - 2 * self.a2 * time,
+                      axe - 2 * self.a2])
+        x = np.linalg.solve(A, b)
+
+        self.a3 = x[0]
+        self.a4 = x[1]
+
+    def calc_point(self, t):
+        xt = self.a0 + self.a1 * t + self.a2 * t ** 2 + \
+             self.a3 * t ** 3 + self.a4 * t ** 4
+
+        return xt
+
+    def calc_first_derivative(self, t):
+        xt = self.a1 + 2 * self.a2 * t + \
+             3 * self.a3 * t ** 2 + 4 * self.a4 * t ** 3
+
+        return xt
+
+    def calc_second_derivative(self, t):
+        xt = 2 * self.a2 + 6 * self.a3 * t + 12 * self.a4 * t ** 2
+
+        return xt
+
+    def calc_third_derivative(self, t):
+        xt = 6 * self.a3 + 24 * self.a4 * t
+
+        return xt
+
+# frenet coordinate
+class FrenetPath:
+
+    def __init__(self):
+        self.t = []
+        self.d = []
+        self.d_d = []
+        self.d_dd = []
+        self.d_ddd = []
+        self.s = []
+        self.s_d = []
+        self.s_dd = []
+        self.s_ddd = []
+        self.cd = 0.0
+        self.cv = 0.0
+        self.cf = 0.0
+
+        self.x = []
+        self.y = []
+        self.yaw = []
+        self.ds = []
+        self.c = []
+
+# optimiaziont step 1
+def calc_frenet_paths(c_speed, c_d, c_d_d, c_d_dd, s0):
+    frenet_paths = []
+
+    # generate path to each offset goal
+    for di in np.arange(-MAX_ROAD_WIDTH, MAX_ROAD_WIDTH, D_ROAD_W):
+
+        # Lateral motion planning
+        for Ti in np.arange(MIN_T, MAX_T, DT):
+            fp = FrenetPath()
+
+            # lat_qp = quintic_polynomial(c_d, c_d_d, c_d_dd, di, 0.0, 0.0, Ti)
+            lat_qp = QuinticPolynomial(c_d, c_d_d, c_d_dd, di, 0.0, 0.0, Ti)
+
+            fp.t = [t for t in np.arange(0.0, Ti, DT)]
+            fp.d = [lat_qp.calc_point(t) for t in fp.t]
+            fp.d_d = [lat_qp.calc_first_derivative(t) for t in fp.t]
+            fp.d_dd = [lat_qp.calc_second_derivative(t) for t in fp.t]
+            fp.d_ddd = [lat_qp.calc_third_derivative(t) for t in fp.t]
+
+            # Longitudinal motion planning (Velocity keeping)
+            for tv in np.arange(TARGET_SPEED - D_T_S * N_S_SAMPLE,
+                                TARGET_SPEED + D_T_S * N_S_SAMPLE, D_T_S):
+                tfp = copy.deepcopy(fp)
+                lon_qp = QuarticPolynomial(s0, c_speed, 0.0, tv, 0.0, Ti)
+
+                tfp.s = [lon_qp.calc_point(t) for t in fp.t]
+                tfp.s_d = [lon_qp.calc_first_derivative(t) for t in fp.t]
+                tfp.s_dd = [lon_qp.calc_second_derivative(t) for t in fp.t]
+                tfp.s_ddd = [lon_qp.calc_third_derivative(t) for t in fp.t]
+
+                Jp = sum(np.power(tfp.d_ddd, 2))  # square of jerk
+                Js = sum(np.power(tfp.s_ddd, 2))  # square of jerk
+
+                # square of diff from target speed
+                ds = (TARGET_SPEED - tfp.s_d[-1]) ** 2
+
+                tfp.cd = K_J * Jp + K_T * Ti + K_D * tfp.d[-1] ** 2
+                tfp.cv = K_J * Js + K_T * Ti + K_D * ds
+                tfp.cf = K_LAT * tfp.cd + K_LON * tfp.cv
+
+                frenet_paths.append(tfp)
+
+    return frenet_paths
+
+# optimization step 2 
+def calc_global_paths(fplist, current_x, current_y):
+    for fp in fplist:
+
+        # calc global positions
+        for i in range(len(fp.s)):
+            ix = current_x
+            iy = current_y
+            if ix is None:
+                break
+            i_yaw = math.atan2(current_y, current_x)
+            di = fp.d[i]
+            fx = ix + di * math.cos(i_yaw + math.pi / 2.0)
+            fy = iy + di * math.sin(i_yaw + math.pi / 2.0)
+            fp.x.append(fx)
+            fp.y.append(fy)
+
+        # calc yaw and ds
+        for i in range(len(fp.x) - 1):
+            dx = fp.x[i + 1] - fp.x[i]
+            dy = fp.y[i + 1] - fp.y[i]
+            fp.yaw.append(math.atan2(dy, dx))
+            fp.ds.append(math.hypot(dx, dy))
+
+        fp.yaw.append(fp.yaw[-1])
+        fp.ds.append(fp.ds[-1])
+
+        # calc curvature
+        for i in range(len(fp.yaw) - 1):
+            fp.c.append((fp.yaw[i + 1] - fp.yaw[i]) / (fp.ds[i]+0.01))
+
+    return fplist
+
+# optimiaization full:
+def frenet_optimal_planning(s0, c_speed, c_d, c_d_d, c_d_dd, current_x, current_y, ob):
+    fplist = calc_frenet_paths(c_speed, c_d, c_d_d, c_d_dd, s0)
+    fplist = calc_global_paths(fplist, current_x, current_y)
+    fplist = check_paths(fplist, ob)
+
+    # find minimum cost path
+    min_cost = float("inf")
+    best_path = None
+    for fp in fplist:
+        if min_cost >= fp.cf:
+            min_cost = fp.cf
+            best_path = fp
+
+    return best_path
+
+def check_collision(fp, ob):
+    for i in range(len(ob[:, 0])):
+        d = [((ix - ob[i, 0]) ** 2 + (iy - ob[i, 1]) ** 2)
+             for (ix, iy) in zip(fp.x, fp.y)]
+
+        collision = any([di <= 30 ** 2 for di in d])
+
+        if collision:
+            return False
+
+    return True
+
+def check_paths(fplist, ob):
+    ok_ind = []
+    for i, _ in enumerate(fplist):
+        if any([v > MAX_SPEED for v in fplist[i].s_d]):  # Max speed check
+            continue
+        elif any([abs(a) > MAX_ACCEL for a in
+                  fplist[i].s_dd]):  # Max accel check
+            continue
+        elif any([abs(c) > MAX_CURVATURE for c in
+                  fplist[i].c]):  # Max curvature check
+            continue
+        elif not check_collision(fplist[i], ob):  # Collision check
+            continue
+
+        ok_ind.append(i)
+
+    return [fplist[i] for i in ok_ind]
+
 
 class Obstacle:
     def __init__(self):
@@ -125,8 +382,8 @@ class MyLocalPlanner(object):
                         ob.vy = actor.get_velocity().y
                         ob.vz = actor.get_velocity().z
                         ob.bbox = actor.bounding_box # in local frame
-                        # print("x: {}, y: {}, z:{}").format(x, y, z)
-                        # print("bbox x:{} y:{} z:{} ext: {} {} {}".format(ob.bbox.location.x, ob.bbox.location.y, ob.bbox.location.z, ob.bbox.extent.x, ob.bbox.extent.y, ob.bbox.extent.z))
+                        print("x: {}, y: {}, z:{}").format(x, y, z)
+                        print("bbox x:{} y:{} z:{} ext: {} {} {}".format(ob.bbox.location.x, ob.bbox.location.y, ob.bbox.location.z, ob.bbox.extent.x, ob.bbox.extent.y, ob.bbox.extent.z))
                         self._obstacles.append(ob)
 
     def check_obstacle(self, point, obstacle):
@@ -277,17 +534,49 @@ class MyLocalPlanner(object):
                 else:
                     break
 
-        # current vehicle waypoint
+        # current position of the car
+        point = Point()
         self._current_waypoint = self.get_waypoint(current_pose.position)
+        car_x = self._current_waypoint.pose.position.x
+        car_y = self._current_waypoint.pose.position.y
+        point.x = self._current_waypoint.pose.position.x
+        point.y = self._current_waypoint.pose.position.y
 
-        # get a list of obstacles surrounding the ego vehicle
+        # position of the obstalces
         self.get_obstacles(current_pose.position, 70.0)
+        obstacle = []
+        for ob in self._obstacles:
+            obs_x = ob.bbox.location.x
+            obs_y = ob.bbox.location.y
+            obstacle = [ob.bbox.location.x, ob.bbox.location.y]
+            
+
+        # conditions of the car
+        c_speed = 10.0 / 3.6  # current speed [m/s]
+        c_d = 2.0  # current lateral position [m]
+        c_d_d = 0.0  # current lateral speed [m/s]
+        c_d_dd = 0.0  # current lateral acceleration [m/s]
+        s0 = self._current_waypoint.s  # current course position
+
+        for i in range(SIM_LOOP):
+            path = frenet_optimal_planning(s0, c_speed, c_d, c_d_d, c_d_dd, car_x, car_y, obstacle)
+            s0 = path.s[1]
+            c_d = path.d[1]
+            c_d_d = path.d_d[1]
+            c_d_dd = path.d_dd[1]
+            c_speed = path.s_d[1]
+
+            # print("c_d: ", c_d, "c_dd: ", c_d_d, "c_ddd: ", c_d_dd, "c_speed: ", c_speed)
 
         # # Example 1: get two waypoints on the left and right lane marking w.r.t current pose
         # left, right = self.get_coordinate_lanemarking(current_pose.position)
         # print("\x1b[6;30;33m------Example 1------\x1b[0m")
         # print("Left: {}, {}; right: {}, {}".format(left.x, left.y, right.x, right.y))
         
+
+        # print("yaw:", self._vehicle_yaw, "current_waypoint: ", self._current_waypoint, "current_speed: ", self._current_speed)
+        # print("obs: ", self._obstacles)
+
         # # Example 2: check obstacle collision
         # print("\x1b[6;30;33m------Example 2------\x1b[0m")
         # point = Point()
@@ -297,32 +586,42 @@ class MyLocalPlanner(object):
         # for ob in self._obstacles:
         #     print("id: {}, collision: {}".format(ob.id, self.check_obstacle(point, ob)))
         
-        # target waypoint
-        self.target_route_point = self._waypoint_buffer[0]
+            # target waypoint
+            self.target_route_point = self._waypoint_buffer[0]
+            print("waypoint_buffer: ", self._waypoint_buffer)
+            target_point = PointStamped()
+            target_point.header.frame_id = "map"
 
-        target_point = PointStamped()
-        target_point.header.frame_id = "map"
-        target_point.point.x = self.target_route_point.position.x
-        target_point.point.y = self.target_route_point.position.y
-        target_point.point.z = self.target_route_point.position.z
-        self._target_point_publisher.publish(target_point)
-        
-        # move using PID controllers
-        control = self._vehicle_controller.run_step(
-            target_speed, current_speed, current_pose, self.target_route_point)
+            # for ob in self._obstacles:
+            #     obs_x = ob.bbox.location.x
 
-        # purge the queue of obsolete waypoints
-        max_index = -1
+            #     # meet obstalce
+            #     if self.check_obstacle(point, ob):
+            #         self.target_route_point.position.x = path.x[1]
+            #         self.target_route_point.position.y = path.y[1]
 
-        sampling_radius = target_speed * 1 / 3.6  # 1 seconds horizon
-        min_distance = sampling_radius * self.MIN_DISTANCE_PERCENTAGE
+            target_point.point.x = self.target_route_point.position.x
+            target_point.point.y = self.target_route_point.position.y
+            target_point.point.z = self.target_route_point.position.z
+            self._target_point_publisher.publish(target_point)    
+                    
+                
+            # move using PID controllers
+            control = self._vehicle_controller.run_step(
+                target_speed, current_speed, current_pose, self.target_route_point)
 
-        for i, route_point in enumerate(self._waypoint_buffer):
-            if distance_vehicle(
-                    route_point, current_pose.position) < min_distance:
-                max_index = i
-        if max_index >= 0:
-            for i in range(max_index + 1):
-                self._waypoint_buffer.popleft()
+            # purge the queue of obsolete waypoints
+            max_index = -1
 
-        return control, False
+            sampling_radius = target_speed * 1 / 3.6  # 1 seconds horizon
+            min_distance = sampling_radius * self.MIN_DISTANCE_PERCENTAGE
+
+            for i, route_point in enumerate(self._waypoint_buffer):
+                if distance_vehicle(
+                        route_point, current_pose.position) < min_distance:
+                    max_index = i
+            if max_index >= 0:
+                for i in range(max_index + 1):
+                    self._waypoint_buffer.popleft()
+
+            return control, False
